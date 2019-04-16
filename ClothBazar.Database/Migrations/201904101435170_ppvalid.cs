a89fc5f755a0b92cf3ea97556a2077307184dcdb
Priyen Brahmbhatt <priyen4@gmail.com>
2019-04-15 23:08:58 -0400

    Project COmpleted

---
+++ ClothBazar.Database/Migrations/201904101435170_ppvalid.cs
@@ -0,0 +1,16 @@
+namespace ClothBazar.Database.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class ppvalid : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
